5b92eb119db18dd21d5e87bafdd66cda5ba6e6ba
Andres Ortiz <3283@holbertonschool.com>
2021-07-24 18:49:57 -0700

    Printf update

---
+++ _printf1.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int i;
+	int ck = 0, counter = 0;
+
+	if (format)
+	{
+		va_start(list, format);
+	for(i = 0; format[i] ; i++)
+	{
+		if(!ck)
+		{
+			if (format[i] != '%')
+			{
+				counter += _putchar(format[i]);
+			}
+			else
+				ck = 1;
+		}
+		else
+		{
+			switch(format[i])
+			{
+			case 'c':
+				counter += _putchar(va_arg(list, int));
+				break;
+			case 's':
+				counter += _string_print(va_arg(list, char *));
+				break;
+			case '%':
+				counter += _putchar('%');
+				break;
+			case '\n':
+				_putchar(10);
+				break;
+			}
+			ck = 0;
+		}
+	}
+	va_end(list);
+	}
+	return(i);
+}
--- mainn.c
+++
@@ -1,7 +0,0 @@
-#include "holberton.h"
-int main(void)
-{
-	_printf("hola %f %s %c %%");
-	_putchar(10);
-	return (0);
-}
